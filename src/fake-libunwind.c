3f515213a418a949542ab1f601bb05feb98b7f31
Chris Diamand <chris@diamand.org>
2015-04-12 00:03:42 +0100

    Remove invalid assumption in stack frame sanity check.

--- src/fake-libunwind.c
+++ src/fake-libunwind.c
@@ -109,8 +109,7 @@ int unw_step(unw_cursor_t *cp)
 	};
 		
 	// sanity check the results
-	if (new_ctxt.frame_sp >= BEGINNING_OF_STACK
-	||  new_ctxt.frame_sp <= (BEGINNING_OF_STACK - 0x100000000))
+	if (new_ctxt.frame_sp >= BEGINNING_OF_STACK)
 	{
 		// looks dodgy -- say we failed
 		return -1;
